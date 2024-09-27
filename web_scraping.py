fa66710e25acc8c12ec6c5060f1a39d11dd814c4
Locatelli10 <nathan.locatelli10@gmail.com>
2024-09-27 17:32:27 -0300

    update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import Select
 
 # Função para download do arquivo
 def download_arquivo():
@@ -33,9 +33,28 @@ def download_arquivo():
         navegador.get(url_anatel)
         # Esperando carregamento do próximo elemento.
         WebDriverWait(navegador, 60).until(EC.presence_of_element_located((By.ID, "filtros_adicionais")))
-        # Abrindo filtros adicionais
+        # Abrindo "filtros adicionais"
         navegador.find_element(By.ID, "filtros_adicionais").click()
-        time.sleep(5)
+        # Esperando carregamento do próximo elemento.
+        WebDriverWait(navegador, 60).until(EC.presence_of_element_located((By.ID, "fa_gsearch")))
+        # Abrindo dropdown "Search by" e selecionando filtro por estado (UF)
+        Select(navegador.find_element(By.ID, "fa_gsearch")).select_by_index(1)
+        # Esperando carregamento do próximo elemento.
+        WebDriverWait(navegador, 60).until(EC.presence_of_element_located((By.ID, "fa_uf")))
+        # Abrindo dropdown "fa_uf" e selecionando o estado (UF)
+        Select(navegador.find_element(By.ID, "fa_uf")).select_by_value("ES")
+        # Esperando carregamento do próximo elemento.
+        WebDriverWait(navegador, 60).until(EC.presence_of_element_located((By.ID, "import")))
+        # Clicando em enviar
+        navegador.find_element(By.ID, "import").click()
+        # Esperando a caixa de pesquisa desaparecer
+        WebDriverWait(navegador, 60).until(EC.invisibility_of_element_located(By.ID, "wait_box"))
+        
+
+
+
+
+        time.sleep(15)
 
         # Encerrando...
         navegador.quit()
